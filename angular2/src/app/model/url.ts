73ac10a8c1959c186713a0a018634734594bc619
Pierre <Pierre@GIR>
2016-11-17 23:05:53 +0200

    Remove the local host reference from the URL.

--- angular2/src/app/model/url.ts
+++ angular2/src/app/model/url.ts
@@ -2,8 +2,8 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class Url {
-    public logbook: string = 'http://localhost:5000/api/logbook';
-    public login: string = 'http://localhost:5000/api/user/signin';
-    public logout: string = 'http://localhost:5000/api/user/signout';
-    public user: string = 'http://localhost:5000/api/user';
+    public logbook: string = 'api/logbook';
+    public login: string = 'api/user/signin';
+    public logout: string = 'user/signout';
+    public user: string = 'api/user';
 }
